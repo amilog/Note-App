20209b392032dad84ccd1bc461503e83df9abf51
Amil Gasimov <93605485+amilgasimov@users.noreply.github.com>
2023-01-13 02:40:49 +0400

    background image added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:note2f/screens/note_editor.dart';
 import 'package:note2f/screens/note_reader.dart';
-import 'package:note2f/style/app_style.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:note2f/widget/note_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,67 +16,94 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppStyle.mainColor,
-      appBar: AppBar(
-        elevation: 0,
-        title:  Text('Note 2 F',style: GoogleFonts.vt323(fontWeight: FontWeight.bold , fontSize: 30),),
-        centerTitle: true,
-        backgroundColor: AppStyle.mainColor,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Your Notes',
-              style: GoogleFonts.roboto(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
-              ),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance.collection("Notes").snapshots(),
-                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  if (snapshot.hasData) {
-                    return GridView(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                      children: snapshot.data!.docs
-                          .map((note) => noteCard(() {
-                                Navigator.push(
-                                    context, MaterialPageRoute(builder: (context) => NoteReaderScreen(note)));
-                              }, note))
-                          .toList(),
-                    );
-                  }
-                  return Text(
-                    'Heç bir Notunuz Yoxdur!',
-                    style: GoogleFonts.nunito(color: Colors.white),
-                  );
-                },
-              ),
-            ),
-          ],
+    return /*Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.topRight, end: Alignment.bottomRight, colors: [
+        Color.fromARGB(255, 4, 11, 88),
+        Color.fromARGB(255, 104, 5, 99),
+        Color.fromARGB(255, 79, 4, 46),
+        Color.fromARGB(255, 102, 13, 6),
+        Color.fromARGB(255, 113, 85, 28),
+        Color.fromARGB(255, 132, 121, 18),
+        Color.fromARGB(255, 5, 153, 173)
+      ])),
+      child:*/
+
+        Stack(
+      children: <Widget>[
+        Image.asset(
+          "assets/backgraund.png",
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.fill,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => NoteEditorScreen()));
-        },
-        child: Icon(Icons.add),
-      ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            elevation: 0,
+            title: Text(
+              'Note 2 F',
+              style: TextStyle(fontFamily: 'RubikVinly', fontSize: 28, color: Colors.yellow[800]),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Your Notes',
+                  style: GoogleFonts.roboto(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  child: StreamBuilder<QuerySnapshot>(
+                    stream: FirebaseFirestore.instance.collection("Notes").snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (snapshot.hasData) {
+                        return GridView(
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                          children: snapshot.data!.docs
+                              .map((note) => noteCard(() {
+                                    Navigator.push(
+                                        context, MaterialPageRoute(builder: (context) => NoteReaderScreen(note)));
+                                  }, note))
+                              .toList(),
+                        );
+                      }
+                      return Text(
+                        'Heç bir Notunuz Yoxdur!',
+                        style: GoogleFonts.nunito(color: Colors.white),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => NoteEditorScreen()));
+            },
+            child: Icon(Icons.add),
+            backgroundColor: Colors.yellow[800],
+          ),
+        ),
+      ],
     );
   }
 }
